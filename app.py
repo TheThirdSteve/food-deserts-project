90e0099600112783e3ae8543275dbb3a83c2d773
TheThirdSteve <50251376+TheThirdSteve@users.noreply.github.com>
2024-11-16 09:16:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import os
  
 import dash_html_components as html
  
@@ -6,10 +7,17 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 server = app.server
  
- 
-app.layout = html.H1(children='Hello World!')
- 
- 
+
+l = []
+for filename in os.listdir(directory):
+    filepath = os.path.join(directory, filename)
+    l.append(filepath)
+
+tex = "/n".join(l)
+
+app.layout = html.H1(children=[os.getcwd(), tex])
+
+
 if __name__ == '__main__':
  
     app.run_server(debug=True)
