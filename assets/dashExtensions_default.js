9b17d6ed1a9109c1e24ec0e6db88ce0ae2626ab3
Joshua Farina <joshuapfarina@gmail.com>
2024-11-24 10:55:19 -0500

    d

--- assets/dashExtensions_default.js
+++ assets/dashExtensions_default.js
@@ -8,9 +8,14 @@ window.dashExtensions = Object.assign({}, window.dashExtensions, {
                 colorProp
             } = context.hideout; // get props from hideout
             const value = feature.properties[colorProp]; // get value that determines the color
-            for (let i = 0; i < classes.length; ++i) {
-                if (value > classes[i]) {
-                    style.fillColor = colorscale[i]; // set the fill color according to the class
+            if (value == null) {
+                style.fillColor = '#808080';
+                return style;
+            }
+            for (let i = 0; i < classes.length - 1; i++) {
+                if (value >= classes[i] && value <= classes[i + 1]) {
+                    style.fillColor = colorscale[i];
+                    break;
                 }
             }
             return style;
